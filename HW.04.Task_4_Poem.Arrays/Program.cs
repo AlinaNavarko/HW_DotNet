8411ad52f010cc8b19ce6b1f9cd73f4d9fa51fe7
Alina Navarko <anavarko@aras.com>
2021-06-23 17:11:41 +0300

    HW_04: added draft for task4

---
+++ HW.04.Task_4_Poem.Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW._04.Task_4_Poem.Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a text of a poem in one line. Use ';' to separate rows of the poem");
+            string[] arrayPoem = new string[1];
+            for (int i = 0; i < arrayPoem.Length ; i++)
+            {
+                arrayPoem[i] = Console.ReadLine();
+                Console.WriteLine(arrayPoem[i]);
+            }
+                        
+            string[] arrayPoem2 = new string[1];
+            for (int i = 0; i < arrayPoem.Length; i++)
+            {
+                arrayPoem[i] = Console.ReadLine();
+                Console.WriteLine(arrayPoem[i]);
+            }
+        }
+    }
+}
