c528c31c31f0969a2954fb926ed3d61df0398355
Alina Navarko <anavarko@aras.com>
2021-06-25 21:01:55 +0300

    HW_5: created draft project

---
+++ HW_05.Task1.Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace HW_05.Task1.Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // string str = "gdfgdf234dg54gf*23oP42";
+        }
+    }
+}
