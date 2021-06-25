0d1b3642311c435053006b09640fef0de6d18f0f
Alina Navarko <anavarko@aras.com>
2021-06-26 01:10:22 +0300

    HW_05: added Task1

--- HW_05.Task1.Strings/Program.cs
+++ HW_05.Task1.Strings/Program.cs
@@ -7,8 +7,61 @@ namespace HW_05.Task1.Strings
     {
         static void Main(string[] args)
         {
+            string str = "gdfgdf234dg54gf*23oP42";
 
-            // string str = "gdfgdf234dg54gf*23oP42";
+            char[] symbols = "+-*/".ToCharArray();
+            char[] numbers = "0123456789".ToCharArray();
+            string num1 = "";
+            string num2 = "";
+            char symbol = ' ';
+            int result = 0;
+
+            foreach (char strChar in str)
+                foreach (char symbolChar in symbols)
+                    if (strChar.Equals(symbolChar))
+                        {
+                        symbol = symbolChar;
+                        break;
+                        }
+
+            string[] strArray = str.Split(symbol);
+       
+            foreach (char strChar in strArray[0])
+                foreach (char numberChar in numbers)
+                    if (strChar.Equals(numberChar))
+                        {
+                        num1 += numberChar;
+                        break;
+                        }
+
+            foreach (char strChar in strArray[1])
+                foreach (char numberChar in numbers)
+                     if (strChar.Equals(numberChar))
+                         {
+                         num2 += numberChar;
+                         break;
+                         }
+
+            int numValue1 = Int32.Parse(num1);
+            int numValue2 = Int32.Parse(num2);
+
+            switch (symbol)
+            {
+                case '+':
+                    result = numValue1 + numValue2;
+                    break;
+                case '-':
+                    result = numValue1 - numValue2;
+                    break;
+                case '*':
+                    result = numValue1 * numValue2;
+                    break;
+                case '/':
+                    result = numValue1 / numValue2;
+                    break;
+            }
+            
+            Console.WriteLine($"{numValue1}{symbol}{numValue2}={result}");
         }
     }
 }
