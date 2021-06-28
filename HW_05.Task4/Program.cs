d9b1e1562969353f2b08e043f5696204d54c5465
Alina Navarko <anavarko@aras.com>
2021-06-28 10:29:40 +0300

    HW_05: added Task3

---
+++ HW_05.Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_05.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> zips = new List<string>() {"OH 43071", "OH 43071"};
+
+            String addresses = "123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432";
+
+            String[] addressArray = addresses.Split(',');
+
+            for (int i = 0; i < addressArray.Length; i++)
+            {
+                Console.WriteLine(addressArray[i]);
+            }
+
+            Console.WriteLine(zips);
+
+        }
+    }
+}
