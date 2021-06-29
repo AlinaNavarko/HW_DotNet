d9451d8246e1abe181bc3d02a1314864f5352145
Alina Navarko <anavarko@aras.com>
2021-06-29 12:14:31 +0300

    HW_04: adjusted part of Task5

--- HW.04.Task_5.String/Program.cs
+++ HW.04.Task_5.String/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace HW._04.Task_5.String
 {
@@ -14,29 +15,34 @@ namespace HW._04.Task_5.String
             MethodPart3(str);
             MethodPart4(str);
         }
+
         private static void MethodPart1(string str)
         {
+            StringBuilder sb = new StringBuilder();
             string[] strArray = str.Split(' ');
-
             int positionMax = 0;
+            
             for (int i = 0; i < strArray.Length; i++)
             {
                 if (strArray[i].Length > strArray[positionMax].Length)
                     positionMax = i;
             }
 
-            Console.WriteLine("SUBTASK_1: An array splitted by rows without an element of max length"); // if there several elements of max Length - program will exclude the first of them)
             for (int i = 0; i < strArray.Length; i++)
             {
                 if (i == positionMax) continue;
-                Console.WriteLine(strArray[i]);
+                sb.Append(strArray[i]);
+                sb.Append(" ");
             }
+            sb.Remove(sb.Length - 1, 1);
+            Console.WriteLine("SUBTASK_1: updated string without Max element:"); // if there several elements of max Length - program will exclude the first of them)
+            Console.WriteLine(sb);
         }
 
         private static void MethodPart2(string str)
         {
+            StringBuilder sb = new StringBuilder();
             string[] strArray2 = str.Split(' ');
-
             int positionMax2 = 0;
             int positionMin2 = 0;
 
@@ -44,12 +50,9 @@ namespace HW._04.Task_5.String
             {
                 if (strArray2[i].Length > strArray2[positionMax2].Length)
                     positionMax2 = i;
-
                 if (strArray2[i].Length < strArray2[positionMin2].Length)
                     positionMin2 = i;
             }
-
-            Console.WriteLine("SUBTASK_2: An array splitted by rows where the element of the greatest and lowest length are switched"); // if there several elements of max/min Length - program will use the first of them)
 
             string tempMin = strArray2[positionMin2];
             string tempMax = strArray2[positionMax2];
@@ -57,28 +60,33 @@ namespace HW._04.Task_5.String
             for (int i = 0; i < strArray2.Length; i++)
             {
                 if (i == positionMin2)
-                   Console.WriteLine(tempMax);
+                    sb.Append(tempMax);
                 else if (i == positionMax2)
-                    Console.WriteLine(tempMin);
+                    sb.Append(tempMin);
                 else
-                    Console.WriteLine(strArray2[i]);
+                    sb.Append(strArray2[i]);
+                sb.Append(" ");
             }
+
+            Console.WriteLine("SUBTASK_2: updated string where the elements of the lowest and greatest length are switched:"); // if there several elements of max/min Length - program will use the first of them)
+            sb.Remove(sb.Length - 1, 1);
+            Console.WriteLine(sb);
         }
 
         private static void MethodPart3(string str)
-         {
+        {
             Console.WriteLine("SUBTASK_3:");
             string str3 = str.Replace(" ", string.Empty);
             int amountPunct = str.Count(char.IsPunctuation);
             int amountLetters = str.Count(char.IsLetter);
             Console.WriteLine($"The amount of punctuation marks is: {amountPunct}");
             Console.WriteLine($"The amount of letters is: {amountLetters}");
-         }
+        }
 
         private static void MethodPart4(string str)
         {
             Console.WriteLine("SUBTASK_4:");
-            Array.ForEach(str.Split(' ').OrderByDescending(x => x.Length).ToArray(), x => Console.WriteLine(x));
+            Array.ForEach(str.Split(' ').OrderByDescending(x => x.Length).ToArray(), x => Console.Write(x + " "));
         }
     }
 }
