7e68bdeab7719629a43893ba32c7390f88c30257
Alina Navarko <anavarko@aras.com>
2021-07-08 13:57:32 +0300

    HW_07: added Task1

---
+++ HW_07_Task1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07_Task1
+{
+    class Person
+    {
+        int _age;
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public void SetAge(int n)
+        {
+            Age = n;
+        }
+
+        public void DisplayAge()
+        {
+            Console.WriteLine($"My age is: {Age}");
+        }
+        public virtual void SayHello()
+        {
+            Console.WriteLine($"Hello from {this.GetType()}");
+        }
+    }
+}
---
+++ HW_07_Task1/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07_Task1
+{
+    class Teacher: Person
+    {
+        string _subject;
+        public string Subject { get; } = "Math";
+        public void Explain()
+        {
+            Console.WriteLine($"Explanation of {Subject} begins");
+        }
+        public override void SayHello()
+        {
+            Console.WriteLine($"Hello from {this.GetType()}");
+        }
+    }
+}
