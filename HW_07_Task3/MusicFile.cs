98f943fe64091af261e8669a5d1f21e86f45b32c
Alina Navarko <anavarko@aras.com>
2021-07-10 23:59:47 +0300

    HW_07: added Task3

---
+++ HW_07_Task3/MusicFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07_Task3
+{
+    class MusicFile: File
+    {
+        string _singer;
+        float _length;
+        public string Singer
+        {
+            get { return _singer; }
+            set { Singer = value; }
+        }
+        public float Length
+        {
+            get { return _length; }
+            set { Length = value; }
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Not implemented yet.");
+        }
+
+        public override string RetrieveInformation()
+        {
+            string result = base.RetrieveInformation() +
+                $"Singer: {Singer}\n" +
+                $"Song Length: {Length} seconds\n";
+            return result;
+        }
+    }
+}
---
+++ HW_07_Task3/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace HW_07_Task3
+{
+    class Catalog
+    {
+        static void Main(string[] args)
+        {
+            var music = ArrayOfMusicFiles();
+            var video = ArrayOfVideoFiles();
+            var prog = ArrayOfProgramFiles();
+        }
+        private static ProgramFile[] ArrayOfProgramFiles()
+        {
+            int arrayLength = 5;
+            ProgramFile[] programs = new ProgramFile[arrayLength];
+            for (int i = 0; i < programs.Length; i++)
+            {
+                var program = new ProgramFile();
+                program.Category = "Office";
+                program.Code = Guid.NewGuid();
+                program.Name = "Excel";
+                program.Size = 5_325;
+                programs[i] = program;
+            }
+
+            return programs;
+        }
+
+        private static VideoFile[] ArrayOfVideoFiles()
+        {
+            int arrayLength = 5;
+            VideoFile[] videos = new VideoFile[arrayLength];
+            for (int i = 0; i < videos.Length; i++)
+            {
+                var video = new VideoFile();
+                video.Category = "Comedy";
+                video.Code = Guid.NewGuid();
+                video.Name = "The Mask";
+                video.Size = 8_725.25;
+                video.Director = "Charles Russell";
+                video.MainActor = "Jim Carrey";
+                video.MainActress = "Cameron Diaz";
+                video.Play();
+                video.RetrieveInformation();
+                videos[i] = video;
+            }
+            return videos;
+        }
+
+        private static MusicFile[] ArrayOfMusicFiles()
+        {
+            int arrayLength = 5;
+            MusicFile[] musics = new MusicFile[arrayLength];
+            for (int i = 0; i < musics.Length; i++)
+            {
+                var music = new MusicFile();
+                music.Category = "Pop";
+                music.Code = Guid.NewGuid();
+                music.Name = "Paparazzi";
+                music.Size = 7.92;
+                music.Length = 192;
+                music.Singer = "Lady Gaga";
+                musics[i] = music;
+            }
+            return musics;
+        }
+    }
+}
